cdb734bbb4001c37d98c3ca2d831f1003945825c
UnBanj <ubanjanin@gmail.com>
2023-11-27 12:57:11 +0100

    added HomePage

---
+++ my-app/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../src/components/Navigation";
+
+const Home = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Welcome dear customer!</h1>
+    </div>
+  );
+};
+
+export default Home;
